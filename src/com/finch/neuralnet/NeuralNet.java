b3476fa43ce9c777beda83934aff9f979475b2a8
Tom <fincht96@gmail.com>
2019-05-10 20:31:16 +0100

    Initial neural net framework

---
+++ src/com/finch/neuralnet/NeuralNet.java
@@ -0,0 +1,36 @@
+package com.finch.neuralnet;
+
+public class NeuralNet {
+
+	/**
+	 * Set number of input, hidden and output nodes (neurons)
+	 */
+	public void init()
+	{
+		
+	}
+	
+	
+	/**
+	 * Refine the weights after being given a training set to learn from
+	 */
+	public void train()
+	{
+		
+	}
+	
+	
+	/**
+	 * Give an answer from output nodes after being given an input
+	 */
+	public void query()
+	{
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
